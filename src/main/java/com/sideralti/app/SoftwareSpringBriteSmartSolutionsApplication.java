fe8775e0186827a7beeb85ad86ce851983774b64
Sideral TI <sideralti.app@gmail.com>
2025-01-23 14:38:27 -0300

    initial project

---
+++ src/main/java/com/sideralti/app/SoftwareSpringBriteSmartSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.sideralti.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareSpringBriteSmartSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareSpringBriteSmartSolutionsApplication.class, args);
+	}
+
+}
